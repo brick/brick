96cf468de60085d7e6404bf7043615f05030cd1b
Benjamin Morel <benjamin.morel@gmail.com>
2013-09-14 01:41:53 +0100

    Simplified the way RequestBody reads php://input stream, which was causing trouble

--- src/Brick/Http/RequestBody.php
+++ src/Brick/Http/RequestBody.php
@@ -14,19 +14,9 @@ class RequestBody
     private $stream;
 
     /**
-     * @var boolean
+     * @var integer
      */
-    private $isEmpty;
-
-    /**
-     * @var boolean
-     */
-    private $isMissing;
-
-    /**
-     * @var string
-     */
-    private $firstByte;
+    private $contentLength;
 
     /**
      * @var string|null
@@ -49,27 +39,10 @@ class RequestBody
     public function __construct($stream, $contentLength)
     {
         $this->stream = $stream;
+        $this->contentLength = $contentLength;
 
         if (ftell($this->stream) != 0) {
             throw RequestBodyException::streamPointerNotAtBeginning();
-        }
-
-        // Read the first byte, to know whether the stream is empty.
-        $this->firstByte = fread($stream, 1);
-
-        $this->isEmpty = ($this->firstByte === '');
-        $this->isMissing = $this->isEmpty && ($contentLength != 0);
-    }
-
-    /**
-     * @return void
-     *
-     * @throws \Brick\Http\RequestBodyException
-     */
-    private function checkReadNotStarted()
-    {
-        if ($this->readStarted) {
-            throw RequestBodyException::readAlreadyStarted();
         }
     }
 
@@ -78,97 +51,18 @@ class RequestBody
      */
     public function isEmpty()
     {
-        return $this->isEmpty;
-    }
-
-    /**
-     * @param integer $length
-     * @return string
-     * @throws \InvalidArgumentException         If the length is not a positive integer.
-     * @throws \Brick\Http\RequestBodyException If the request body is not available.
-     */
-    public function read($length)
-    {
-        if ($this->isMissing) {
-            throw RequestBodyException::noRequestBody();
-        }
-
-        $length = (int) $length;
-
-        if ($length <= 0) {
-            throw new \InvalidArgumentException('Length parameter must be greater than 0');
-        }
-
-        $this->readStarted = true;
-        $data = $this->firstByte;
-
-        if ($this->firstByte !== '') {
-            $this->firstByte = '';
-            $length--;
-        }
-
-        while ($length != 0 && $this->hasMoreData()) {
-            $block = fread($this->stream, $length);
-            $data .= $block;
-            $length -= strlen($block);
-        }
-
-        return $data;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function hasMoreData()
-    {
-        return ! feof($this->stream);
-    }
-
-    /**
-     * @param resource $stream
-     * @return void
-     */
-    public function copyToStream($stream)
-    {
-        $this->checkReadNotStarted();
-
-        while ($this->hasMoreData()) {
-            fwrite($stream, $this->read(8192));
-        }
+        return $this->toString() !== '';
     }
 
     /**
      * @return string
-     *
-     * @throws \Brick\Http\RequestBodyException
      */
     public function toString()
     {
-        if ($this->content !== null) {
-            return $this->content;
-        }
-
-        if (! $this->hasMoreData()) {
-            throw RequestBodyException::requestBodyAlreadyRead();
-        }
-
-        $this->checkReadNotStarted();
-        $this->content = '';
-
-        while ($this->hasMoreData()) {
-            $this->content .= $this->read(8192);
+        if ($this->content === null) {
+            $this->content = stream_get_contents($this->stream);
         }
 
         return $this->content;
-    }
-
-    /**
-     * @return array
-     */
-    public function getFormData()
-    {
-        parse_str($this->toString(), $data);
-
-        return $data;
     }
 }
--- src/Brick/Http/RequestBodyException.php
+++ src/Brick/Http/RequestBodyException.php
@@ -10,40 +10,8 @@ class RequestBodyException extends \RuntimeException
     /**
      * @return RequestBodyException
      */
-    public static function noRequestBody()
-    {
-        return new self(
-            'The request body is not available. ' .
-            'PHP currently hides it when the Content-Type is set to multipart/form-data. ' .
-            'This can also happen when it has already been read outside the Request object: ' .
-            'the request body is not supposed to be read more than once.'
-        );
-    }
-
-    /**
-     * @return RequestBodyException
-     */
-    public static function requestBodyAlreadyRead()
-    {
-        return new self(
-            'The request body has already been read as a stream. ' .
-            'It cannot be read more than once.'
-        );
-    }
-
-    /**
-     * @return RequestBodyException
-     */
     public static function streamPointerNotAtBeginning()
     {
         return new self('The stream pointer is not at the beginning of the stream.');
-    }
-
-    /**
-     * @return RequestBodyException
-     */
-    public static function readAlreadyStarted()
-    {
-        return new self('Cannot get the body contents: read has already started');
     }
 }
