946d6120c2c5080c3faaa74f9c24f528157ae38a
Benjamin Morel <benjamin.morel@gmail.com>
2013-10-17 09:37:26 +0100

    Removed an @throws in Brick\Routing\Route that's not relevant to implementors

--- src/Brick/Routing/Route.php
+++ src/Brick/Routing/Route.php
@@ -16,7 +16,6 @@ interface Route
      *
      * @return \Brick\Routing\RouteMatch|null A match, or null if no match is found.
      *
-     * @throws \Brick\Routing\RoutingException     If a problem occurs after a match has been found.
      * @throws \Brick\Http\Exception\HttpException A route is allowed to throw HTTP exceptions.
      */
     public function match(Request $request);
