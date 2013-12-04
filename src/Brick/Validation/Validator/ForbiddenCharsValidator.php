31f8c838d60ff370dc44fa188c8a050d374bd937
Benjamin Morel <benjamin.morel@gmail.com>
2013-12-04 16:02:02 +0000

    Added a ForbiddenCharsValidator

---
+++ src/Brick/Validation/Validator/ForbiddenCharsValidator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Brick\Validation\Validator;
+
+use Brick\Validation\Validator;
+use Brick\Validation\ValidationResult;
+
+/**
+ * Validates that a string does not contain forbidden characters.
+ */
+class ForbiddenCharsValidator implements Validator
+{
+    /**
+     * @var string
+     */
+    private $forbiddenChars;
+
+    /**
+     * Class constructor.
+     *
+     * @param string $forbiddenChars A string containing all forbidden characters.
+     */
+    public function __construct($forbiddenChars)
+    {
+        $this->forbiddenChars = (string) $forbiddenChars;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value)
+    {
+        if (! is_null($value) && ! is_string($value)) {
+            throw new \InvalidArgumentException('Value must be a string or null');
+        }
+
+        $result = new ValidationResult();
+        $regexp = '/[' . preg_quote($this->forbiddenChars, '/') . ']/';
+
+        if (preg_match($regexp, $value) != 0) {
+            $result->addFailure('validator.forbidden.chars');
+        }
+
+        return $result;
+    }
+}
