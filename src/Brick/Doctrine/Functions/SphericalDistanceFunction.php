ac25c17d5633a7068576b76304b04d426672229a
Benjamin Morel <benjamin.morel@gmail.com>
2014-07-30 22:19:03 +0200

    Refactored SphericalDistanceFunction

--- src/Brick/Doctrine/Functions/SphericalDistanceFunction.php
+++ src/Brick/Doctrine/Functions/SphericalDistanceFunction.php
@@ -13,6 +13,13 @@ use Doctrine\ORM\Query\Parser;
 class SphericalDistanceFunction extends FunctionNode
 {
     /**
+     * The great-circle distance formula.
+     *
+     * @const string
+     */
+    const FORMULA = '(%s * ACOS(SIN(%s) * SIN(%s) + COS(%s) * COS(%s) * COS(%s - %s)))';
+
+    /**
      * The quadratic mean approximation of the average
      * great-circle circumference of the Earth, in meters.
      *
@@ -31,81 +38,19 @@ class SphericalDistanceFunction extends FunctionNode
     private $arg2;
 
     /**
-     * @param string $value
-     *
-     * @return string
-     */
-    private static function x($value)
-    {
-        return sprintf('RADIANS(X(%s))', $value);
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return string
-     */
-    private static function y($value)
-    {
-        return sprintf('RADIANS(Y(%s))', $value);
-    }
-
-    /**
-     * @param \Doctrine\ORM\Query\SqlWalker $s
-     *
-     * @return string
-     */
-    private function x1(SqlWalker $s)
-    {
-        return self::x($this->arg1->dispatch($s));
-    }
-
-    /**
-     * @param \Doctrine\ORM\Query\SqlWalker $s
-     *
-     * @return string
-     */
-    private function x2(SqlWalker $s)
-    {
-        return self::x($this->arg2->dispatch($s));
-    }
-
-    /**
-     * @param \Doctrine\ORM\Query\SqlWalker $s
-     *
-     * @return string
-     */
-    private function y1(SqlWalker $s)
-    {
-        return self::y($this->arg1->dispatch($s));
-    }
-
-    /**
-     * @param \Doctrine\ORM\Query\SqlWalker $s
-     *
-     * @return string
-     */
-    private function y2(SqlWalker $s)
-    {
-        return self::y($this->arg2->dispatch($s));
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function getSql(SqlWalker $s)
     {
-        $formula = '%s * ACOS(SIN(%s) * SIN(%s) + COS(%s) * COS(%s) * COS(%s - %s))';
-
         return sprintf(
-            $formula,
+            self::FORMULA,
             self::EARTH_RADIUS,
-            $this->y1($s),
-            $this->y2($s),
-            $this->y1($s),
-            $this->y2($s),
-            $this->x2($s),
-            $this->x1($s)
+            $this->y($this->arg1->dispatch($s)),
+            $this->y($this->arg2->dispatch($s)),
+            $this->y($this->arg1->dispatch($s)),
+            $this->y($this->arg2->dispatch($s)),
+            $this->x($this->arg2->dispatch($s)),
+            $this->x($this->arg1->dispatch($s))
         );
     }
 
@@ -122,5 +67,25 @@ class SphericalDistanceFunction extends FunctionNode
         $this->arg2 = $parser->ArithmeticPrimary();
 
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    private static function x($value)
+    {
+        return sprintf('RADIANS(ST_X(%s))', $value);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    private function y($value)
+    {
+        return sprintf('RADIANS(ST_Y(%s))', $value);
     }
 }
