a5a6fde59fdef9bc1c04d2c0c215b34885144d1f
Benjamin Morel <benjamin.morel@gmail.com>
2014-08-01 18:15:08 +0200

    Fixed Brick\DateTime\Utility\Math::floorDiv() and added tests

--- src/Brick/DateTime/Utility/Math.php
+++ src/Brick/DateTime/Utility/Math.php
@@ -8,10 +8,10 @@ namespace Brick\DateTime\Utility;
 class Math
 {
     /**
-     * Returns the quotient of the division of two integers.
+     * Divides two integers into a quotient and a remainder.
      *
      * @param integer $a The dividend, validated as an integer.
-     * @param integer $b The divisor, validated as an integer.
+     * @param integer $b The divisor, validated as a non-zero integer.
      * @param integer $r An optional variable to store the remainder of the division.
      *
      * @return integer The quotient of the division.
@@ -25,20 +25,30 @@ class Math
      * Returns the largest integer value that is less than or equal to the algebraic quotient.
      *
      * @param integer $a The first argument, validated as an integer.
-     * @param integer $b The second argument, validated as an integer.
+     * @param integer $b The second argument, validated as a non-zero integer.
      *
      * @return integer
      */
     public static function floorDiv($a, $b)
     {
-        return ($a >= 0) ? self::div($a, $b) : self::div($a + 1, $b) - 1;
+        $r = self::div($a, $b);
+
+        // If the signs are different and modulo not zero, round down.
+        if (($a ^ $b) < 0 && ($r * $b != $a)) {
+            $r--;
+        }
+
+        return $r;
     }
 
     /**
-     * Returns the floor modulus of the int arguments.
+     * Returns the floor modulus of the integer arguments.
+     *
+     * The relationship between floorDiv and floorMod is such that:
+     * floorDiv(x, y) * y + floorMod(x, y) == x
      *
      * @param integer $a The first argument, validated as an integer.
-     * @param integer $b The second argument, validated as an integer.
+     * @param integer $b The second argument, validated as a non-zero integer.
      *
      * @return integer
      */
--- src/Brick/Tests/DateTime/Utility/MathTest.php
+++ src/Brick/Tests/DateTime/Utility/MathTest.php
@@ -37,4 +37,54 @@ class MathTest extends \PHPUnit_Framework_TestCase
             [-11, -5,  2, -1],
         ];
     }
+
+    /**
+     * @dataProvider providerFloorDiv
+     *
+     * @param integer $a        The dividend.
+     * @param integer $b        The divisor.
+     * @param integer $expected The expected floor division result.
+     */
+    public function testFloorDiv($a, $b, $expected)
+    {
+        $this->assertSame($expected, Math::floorDiv($a, $b));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerFloorDiv()
+    {
+        return [
+            [ 3,  2,  1],
+            [ 3, -2, -2],
+            [-3,  2, -2],
+            [-3, -2,  1]
+        ];
+    }
+
+    /**
+     * @dataProvider providerFloorMod
+     *
+     * @param integer $a        The dividend.
+     * @param integer $b        The divisor.
+     * @param integer $expected The expected floor modulus result.
+     */
+    public function testFloorMod($a, $b, $expected)
+    {
+        $this->assertSame($expected, Math::floorMod($a, $b));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerFloorMod()
+    {
+        return [
+            [ 3,  2,  1],
+            [ 3, -2, -1],
+            [-3,  2,  1],
+            [-3, -2, -1]
+        ];
+    }
 }
