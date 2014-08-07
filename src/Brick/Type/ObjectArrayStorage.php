7cc07f6762c1cbcfa9fb0c330c19fff3c4237aa0
Benjamin Morel <benjamin.morel@gmail.com>
2014-08-07 15:28:49 +0200

    Refactored ObjectArrayStorage around ObjectStorage, added tests

--- src/Brick/Type/ObjectArrayStorage.php
+++ src/Brick/Type/ObjectArrayStorage.php
@@ -5,34 +5,56 @@ namespace Brick\Type;
 /**
  * Associates an array of values to an object.
  */
-class ObjectArrayStorage implements \IteratorAggregate
+class ObjectArrayStorage implements \Countable, \IteratorAggregate
 {
     /**
-     * @var \SplObjectStorage
+     * The underlying storage.
+     *
+     * @var ObjectStorage
      */
-    private $objects;
+    private $storage;
 
     /**
      * Class constructor.
      */
     public function __construct()
     {
-        $this->objects = new \SplObjectStorage();
+        $this->storage = new ObjectStorage();
     }
 
     /**
+     * Returns whether this storage contains the given object.
+     *
+     * @param object $object The object to test.
+     *
+     * @return boolean True if this storage contains the object, false otherwise.
+     */
+    public function has($object)
+    {
+        return $this->storage->has($object);
+    }
+
+    /**
+     * Returns the values associated to the given object.
+     *
+     * If the object is not present in the storage, an empty array is returned.
+     *
      * @param object $object The object.
      *
      * @return array The values associated with the object.
      */
     public function get($object)
     {
-        return $this->objects->offsetExists($object) ? $this->objects->offsetGet($object) : [];
+        $values = $this->storage->get($object);
+
+        return ($values === null) ? [] : $values;
     }
 
     /**
+     * Adds data associated with the given object.
+     *
      * @param object $object The object.
-     * @param mixed  $value  The value to associate to the object.
+     * @param mixed  $value  The value to add.
      *
      * @return static This ObjectArrayStorage instance.
      */
@@ -40,18 +62,48 @@ class ObjectArrayStorage implements \IteratorAggregate
     {
         $values = $this->get($object);
         $values[] = $value;
-        $this->objects->offsetSet($object, $values);
+        $this->storage->set($object, $values);
 
         return $this;
     }
 
     /**
-     * Required by interface IteratorAggregate.
+     * Removes all values associated with the given object from the storage.
      *
-     * {@inheritdoc}
+     * If this storage does not any value for the given object, this method does nothing.
+     *
+     * @param object $object The object to remove.
+     *
+     * @return static This ObjectStorage instance.
+     */
+    public function remove($object)
+    {
+        $this->storage->remove($object);
+
+        return $this;
+    }
+
+    /**
+     * Returns the number of objects in this storage.
+     *
+     * This method is part of the Countable interface.
+     *
+     * @return integer
+     */
+    public function count()
+    {
+        return $this->storage->count();
+    }
+
+    /**
+     * Returns an iterator for this storage.
+     *
+     * This method is part of the IteratorAggregate interface.
+     *
+     * @return \Generator
      */
     public function getIterator()
     {
-        return $this->objects;
+        return $this->storage->getIterator();
     }
 }
