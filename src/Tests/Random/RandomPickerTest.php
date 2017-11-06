325fcb45967e7987cfe36bfa8421a428d9a6d45b
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-06 16:53:39 +0100

    Fix RandomPicker tests
    The random number generator cannot be passed to the constructor anymore, now that PHP 7 offers a cryptographically secure random_int() function.
    We need to mock the random_int() function for tests to pass.

--- src/Tests/Random/RandomPickerTest.php
+++ src/Tests/Random/RandomPickerTest.php
@@ -1,113 +1,157 @@
 <?php
 
-namespace Brick\Tests\Random;
-
-use Brick\Random\RandomPicker;
-
-use PHPUnit\Framework\TestCase;
-
-/**
- * Tests for class RandomPicker.
- */
-class RandomPickerTest extends TestCase
+namespace Brick\Random
 {
+    class TestData
+    {
+        /**
+         * @var bool
+         */
+        public static $useTestData;
+
+        /**
+         * @var array
+         */
+        public static $randomValues;
+
+        /**
+         * @var int
+         */
+        public static $index;
+    }
+
     /**
-     * @dataProvider providerSeed
+     * @param int $min
+     * @param int $max
      *
-     * @param int $seed
+     * @return int
      */
-    public function testUniformDistribution($seed)
+    function random_int(int $min, int $max) : int
     {
-        srand($seed);
-
-        $elements = [];
-
-        $elementCount = rand(9, 99);
-
-        for ($i = 1; $i <= $elementCount; $i++) {
-            $elements["test $i"] = rand(1, 99);
+        if (TestData::$useTestData) {
+            return TestData::$randomValues[TestData::$index];
         }
 
-        $totalWeight = array_sum($elements);
-        $randValues = range(1, $totalWeight);
-
-        $this->shufflePreserveKeys($elements);
-        shuffle($randValues); // we *do* want keys to be reassigned here
-
-        $randomNumberGenerator = function() use ($randValues, & $index) {
-            return $randValues[$index];
-        };
-
-        $picker = new RandomPicker($randomNumberGenerator);
-        $picker->addElements($elements);
-
-        $results = [];
-
-        foreach ($elements as $element => $weight) {
-            $results[$element] = 0;
-        }
-
-        for ($index = 0; $index < $totalWeight; $index++) {
-            $element = $picker->getRandomElement();
-            $results[$element]++;
-        }
-
-        $this->assertSame($elements, $results);
+        return \random_int($min, $max);
     }
+}
+
+namespace Brick\Tests\Random
+{
+    use Brick\Random\RandomPicker;
+    use Brick\Random\TestData;
+
+    use PHPUnit\Framework\TestCase;
 
     /**
-     * @return array
+     * Tests for class RandomPicker.
      */
-    public function providerSeed()
+    class RandomPickerTest extends TestCase
     {
-        for ($i = 0; $i < 10; $i++) {
-            yield [$i];
-        }
-    }
-
-    public function testDefaultGeneratorSingleElement()
-    {
-        $picker = new RandomPicker();
-        $picker->addElement('test', 123);
-
-        $this->assertSame('test', $picker->getRandomElement());
-        $this->assertSame('test', $picker->getRandomElement());
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     */
-    public function testInvalidWeight()
-    {
-        $picker = new RandomPicker();
-        $picker->addElement('test', 0);
-    }
-
-    /**
-     * @expectedException \RuntimeException
-     */
-    public function testEmptyPicker()
-    {
-        $picker = new RandomPicker();
-        $picker->getRandomElement();
-    }
-
-    /**
-     * Shuffles an associative array, preserving keys.
-     *
-     * @param $array
-     */
-    private function shufflePreserveKeys(array & $array) {
-        $keys = array_keys($array);
-
-        shuffle($keys);
-
-        $result = [];
-
-        foreach ($keys as $key) {
-            $result[$key] = $array[$key];
+        protected function setUp()
+        {
+            TestData::$useTestData = false;
         }
 
-        $array = $result;
+        /**
+         * @dataProvider providerSeed
+         *
+         * @param int $seed
+         */
+        public function testUniformDistribution($seed)
+        {
+            TestData::$useTestData = true;
+
+            srand($seed);
+
+            $elements = [];
+
+            $elementCount = rand(9, 99);
+
+            for ($i = 1; $i <= $elementCount; $i++) {
+                $elements["test $i"] = rand(1, 99);
+            }
+
+            $totalWeight = array_sum($elements);
+            $randValues = range(1, $totalWeight);
+
+            $this->shufflePreserveKeys($elements);
+            shuffle($randValues); // we *do* want keys to be reassigned here
+
+            TestData::$randomValues = $randValues;
+
+            $picker = new RandomPicker();
+            $picker->addElements($elements);
+
+            $results = [];
+
+            foreach ($elements as $element => $weight) {
+                $results[$element] = 0;
+            }
+
+            for ($index = 0; $index < $totalWeight; $index++) {
+                TestData::$index = $index;
+                $element = $picker->getRandomElement();
+                $results[$element]++;
+            }
+
+            $this->assertSame($elements, $results);
+        }
+
+        /**
+         * @return array
+         */
+        public function providerSeed()
+        {
+            for ($i = 0; $i < 10; $i++) {
+                yield [$i];
+            }
+        }
+
+        public function testDefaultGeneratorSingleElement()
+        {
+            $picker = new RandomPicker();
+            $picker->addElement('test', 123);
+
+            $this->assertSame('test', $picker->getRandomElement());
+            $this->assertSame('test', $picker->getRandomElement());
+        }
+
+        /**
+         * @expectedException \InvalidArgumentException
+         */
+        public function testInvalidWeight()
+        {
+            $picker = new RandomPicker();
+            $picker->addElement('test', 0);
+        }
+
+        /**
+         * @expectedException \RuntimeException
+         */
+        public function testEmptyPicker()
+        {
+            $picker = new RandomPicker();
+            $picker->getRandomElement();
+        }
+
+        /**
+         * Shuffles an associative array, preserving keys.
+         *
+         * @param $array
+         */
+        private function shufflePreserveKeys(array & $array) {
+            $keys = array_keys($array);
+
+            shuffle($keys);
+
+            $result = [];
+
+            foreach ($keys as $key) {
+                $result[$key] = $array[$key];
+            }
+
+            $array = $result;
+        }
     }
 }
